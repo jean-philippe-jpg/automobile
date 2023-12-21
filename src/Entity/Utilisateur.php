c2b88c22b331bef7eb772170c8706f27e720f520
champion <jphilippe.champion@gmail.com>
2023-12-21 13:06:47 +0100

    table admin, utilisateur, crud manuelle

---
+++ src/Entity/Utilisateur.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
+class Utilisateur
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $email = null;
+
+    #[ORM\Column]
+    private ?int $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?int
+    {
+        return $this->password;
+    }
+
+    public function setPassword(int $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
